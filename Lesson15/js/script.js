389efc2e01d3378032d65b3907cbcea47bb0faa9
BulyginK <bulygin.k@gmail.com>
2022-04-18 07:34:49 +0300

    выполнил задание 15 урока

--- Lesson15/js/script.js
+++ Lesson15/js/script.js
@@ -1,3 +1,27 @@
 'use strict';
 
 // 1) Создать функцию-конструктор DomElement, который содержит свойства
+
+const DomElement = function(selector) {
+    this.selector = ''
+    this.selector = selector;
+    this.height = height;
+    this.width = width;
+    this.bg = bg;
+    this.fontSize = fontSize;
+}
+
+DomElement.prototype.addDiv = function() {
+    if (selector[0] = '.') {
+        let div = document.createElement('div');
+        div.className = selector.substring(1);
+    } else if (selector[0] = '#') {
+        let paragraph = document.createElement('p');
+        paragraph.id ='igParagraph';
+    }
+}
+
+const elem = new DomElement();
+
+console.log(elem);
+
