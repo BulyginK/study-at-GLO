baa1a64cc21838eb78d9ea461521916954df8b62
BulyginK <buligin.k@gmail.com>
2022-04-18 18:31:28 +0300

    выполнил задание 15 урока

--- Lesson15/js/script.js
+++ Lesson15/js/script.js
@@ -1,27 +1,36 @@
 'use strict';
 
 // 1) Создать функцию-конструктор DomElement, который содержит свойства
+// selector,height,width,bg,fontSize
+// selector, height, width, bg, fontSize, newElement
 
-const DomElement = function(selector) {
-    this.selector = ''
+const body = document.querySelector('body');
+
+const DomElement = function(selector, height, width, bg, fontSize) {
     this.selector = selector;
     this.height = height;
     this.width = width;
-    this.bg = bg;
+    this.backgroundColor = bg;
     this.fontSize = fontSize;
-}
+    this.newElement = function() {
+        selector = prompt("Какой блок создать? .*** - div, #*** - paragraph", "");
+        let element = ''
+        if (selector[0] == '.') {
+            element = document.createElement('div');
+            element.className = selector.substring(1);
+            body.append(element);
+        } else if (selector[0] == '#') {
+            element = document.createElement('p');
+            element.id= selector.substring(1);
+            body.append(element);
+        }
+        height = prompt("Какой ширины создать блок?", "");
+        console.log(height);
 
-DomElement.prototype.addDiv = function() {
-    if (selector[0] = '.') {
-        let div = document.createElement('div');
-        div.className = selector.substring(1);
-    } else if (selector[0] = '#') {
-        let paragraph = document.createElement('p');
-        paragraph.id ='igParagraph';
+        element.style.cssText = 'height: ' + height + ';';
+
     }
 }
 
 const elem = new DomElement();
-
-console.log(elem);
-
+elem.newElement();
