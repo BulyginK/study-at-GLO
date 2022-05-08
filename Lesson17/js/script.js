4e4f439e629c14ef04f958024b815be310dbac1d
BulyginK <bulygin.k@gmail.com>
2022-05-09 00:20:40 +0300

    выполнил задание 17 урока

---
+++ Lesson17/js/script.js
@@ -0,0 +1,21 @@
+'use strict';
+const saveBtn = document.querySelector('#save-btn');
+
+class Object {
+    constructor(namber, type, name, year) {
+        this.namber = namber;
+        this.type = type;
+        this.name = name;
+        this.year = year;
+    }
+
+
+    newObject() {
+        const objectType = document.querySelector('.object__type');
+    }
+}
+
+
+const object = new Object();
+
+object.newObject();
