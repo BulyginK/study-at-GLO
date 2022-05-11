9a1aa3ea12a24edecd52f27419c94398cd03c0f8
BulyginK <bulygin.k@gmail.com>
2022-05-11 06:41:41 +0300

    выполнил задание 17 урока

--- Lesson17/js/script.js
+++ Lesson17/js/script.js
@@ -1,5 +1,14 @@
 'use strict';
 const saveBtn = document.querySelector('#save-btn');
+const objectType = document.querySelector('.object__type');
+const realtyType = document.querySelector('.object__realty__type');
+const realtyName = document.querySelector('.object__realty__name');
+const realtyPurpose = document.querySelector('.object__realty__purpose');
+const realtyYear = document.querySelector('.object__realty__year');
+const realtyKadnumber = document.querySelector('.object__realty__kadnumber');
+const realtyArea = document.querySelector('.object__realty__area');
+const trObjectRealty = document.querySelector('.tr__object__realty');
+let count = 0;
 
 class Object {
     constructor(namber, type, name, year) {
@@ -8,14 +17,38 @@ class Object {
         this.name = name;
         this.year = year;
     }
+}
 
-
-    newObject() {
-        const objectType = document.querySelector('.object__type');
+class ObjectRealty extends Object {
+    constructor(namber, type, name, year, kadnumber, area) {
+        super(namber, type, name, year)
+        this.kadnumber = kadnumber;
+        this.area = area;
     }
 }
 
+const newTr = () => {
+    let newTr = document.createElement('tr');
+    count++;
+    newTr.innerHTML =
+        `
+        <tr>
+            <td>${count}</td>
+            <td>${realtyType.options[realtyType.selectedIndex].textContent}</td>
+            <td>${realtyName.value}</td>
+            <td>${realtyPurpose.value}</td>
+            <td>${realtyYear.value}</td>
+            <td>${realtyKadnumber.value}</td>
+            <td>${realtyArea.value}</td>
+        </tr>
+        `;
+    trObjectRealty.append(newTr);
+}
 
-const object = new Object();
-
-object.newObject();
+saveBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    const type = realtyType.options[realtyType.selectedIndex].textContent;
+    const objectRealty = new ObjectRealty(count, type, realtyPurpose.value, realtyYear.value, realtyKadnumber.value, realtyArea.value);
+    newTr();
+    console.log(objectRealty);
+});
